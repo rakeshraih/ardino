1e33d8374a8d32d1e29dc1c6c471f2fe64a1654b
rakeshraih <rakeshraih@gmail.com>
2023-02-18 19:06:26 -0800

    Create Measure-distance.ino

---
+++ Measure-distance/Measure-distance.ino
@@ -0,0 +1,32 @@
+#include "Adafruit_VL53L0X.h"
+
+Adafruit_VL53L0X lox = Adafruit_VL53L0X();
+
+void setup() {
+  Serial.begin(9600);
+
+  // wait until serial port opens for native USB devices
+  while (! Serial) {
+    delay(1);
+  }
+
+  Serial.println("Adafruit VL53L0X test.");
+  if (!lox.begin()) {
+    Serial.println(F("Failed to boot VL53L0X"));
+    while(1);
+  }
+  // power
+  Serial.println(F("VL53L0X API Continuous Ranging example\n\n"));
+
+  // start continuous ranging
+  lox.startRangeContinuous();
+}
+
+void loop() {
+    Serial.write(27);       // ESC command
+  //if (lox.isRangeComplete()) {
+    Serial.println("Distance in mm: ");
+    Serial.println(lox.readRange());
+  //}
+  delay(1000);
+}
